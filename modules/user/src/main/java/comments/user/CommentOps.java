8e970110b54ddc5499589ec72cfb67fe74b5d3c6
dyu <david.yu.ftw@gmail.com>
2017-05-02 23:47:54 +0800

    reply handling

--- modules/user/src/main/java/comments/user/CommentOps.java
+++ modules/user/src/main/java/comments/user/CommentOps.java
@@ -2,11 +2,21 @@
 
 package comments.user;
 
+import static com.dyuproject.protostuffdb.EntityMetadata.ZERO_KEY;
 import static com.dyuproject.protostuffdb.SerializedValueUtil.readByteArrayOffsetWithTypeAsSize;
 
+import java.io.IOException;
+
+import com.dyuproject.protostuff.KeyBuilder;
+import com.dyuproject.protostuff.Pipe;
+import com.dyuproject.protostuff.RpcHeader;
+import com.dyuproject.protostuff.RpcResponse;
 import com.dyuproject.protostuffdb.DSRuntimeExceptions;
+import com.dyuproject.protostuffdb.Datastore;
 import com.dyuproject.protostuffdb.EntityMetadata;
 import com.dyuproject.protostuffdb.OpChain;
+import com.dyuproject.protostuffdb.RangeV;
+import com.dyuproject.protostuffdb.ValueUtil;
 import com.dyuproject.protostuffdb.WriteContext;
 
 /**
@@ -52,5 +62,88 @@ public final class CommentOps
         return param.provide(now, 
                 append(parentValue, offset, size, parentKey), 
                 depth);
+    }
+    
+    static boolean create(Comment req, 
+            Datastore store, RpcResponse res,
+            Pipe.Schema<Comment.PList> resPipeSchema, 
+            RpcHeader header) throws IOException
+    {
+        final byte[] lastSeenKey = req.key, 
+                parentKey = req.parentKey,
+                key = new byte[9];
+        
+        if (!store.chain(null, XCommentOps.OP_NEW, req, 0, res.context, key))
+            return res.fail("Could not create.");
+        
+        req.key = key;
+        
+        if (parentKey.length != 0)
+        {
+            // user posted a reply
+            final byte[] startKey;
+            if (lastSeenKey == null)
+            {
+                startKey = ValueUtil.copy(req.keyChain, 0, req.keyChain.length - 8);
+            }
+            else
+            {
+                startKey = ValueUtil.copy(req.keyChain, 0, req.keyChain.length);
+                // visit starting the entry after the last seen one
+                lastSeenKey[lastSeenKey.length - 1] |= 0x02;
+                System.arraycopy(lastSeenKey, 0, startKey, startKey.length - 9, 9);
+            }
+            
+            KeyBuilder kb = res.context.kb()
+                    .begin(Comment.IDX_POST_ID__KEY_CHAIN, Comment.EM)
+                    .$append(req.postId)
+                    .$append(startKey)
+                    .$push()
+                    .begin(Comment.IDX_POST_ID__KEY_CHAIN, Comment.EM)
+                    .$append(req.postId)
+                    .$push()
+                    .begin(Comment.IDX_POST_ID__KEY_CHAIN, Comment.EM)
+                    .$append(req.postId)
+                    .$append8(0xFF)
+                    .$push();
+            
+            store.visitRange(false, -1, false, kb.copy(-2), res.context, 
+                    RangeV.RES_PV, res, 
+                    kb.buf(), kb.offset(-1), kb.len(-1), 
+                    kb.buf(), kb.offset(), kb.len());
+        }
+        else if (lastSeenKey != null)
+        {
+            // visit starting the entry after the last seen one
+            lastSeenKey[lastSeenKey.length - 1] |= 0x02;
+            
+            KeyBuilder kb = res.context.kb()
+                    .begin(Comment.IDX_POST_ID__KEY_CHAIN, Comment.EM)
+                    .$append(req.postId)
+                    .$append(ZERO_KEY)
+                    .$append(lastSeenKey)
+                    .$push()
+                    .begin(Comment.IDX_POST_ID__KEY_CHAIN, Comment.EM)
+                    .$append(req.postId)
+                    .$push()
+                    .begin(Comment.IDX_POST_ID__KEY_CHAIN, Comment.EM)
+                    .$append(req.postId)
+                    .$append8(0xFF)
+                    .$push();
+            
+            store.visitRange(false, -1, false, kb.copy(-2), res.context, 
+                    RangeV.RES_PV, res, 
+                    kb.buf(), kb.offset(-1), kb.len(-1), 
+                    kb.buf(), kb.offset(), kb.len());
+        }
+        else
+        {
+            CommentViews.visitByPostId(req.postId,
+                    store, res.context,
+                    RangeV.Store.ENTITY_PV,
+                    RangeV.RES_PV, res);
+        }
+
+        return true;
     }
 }
