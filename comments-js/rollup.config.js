8ffc13be4de37de28edb96c557f8220916ebfba1
dyu <david.yu.ftw@gmail.com>
2017-05-02 12:29:08 +0800

    scss

--- comments-js/rollup.config.js
+++ comments-js/rollup.config.js
@@ -4,11 +4,12 @@ import commonjs from 'rollup-plugin-commonjs';
 import buble from 'rollup-plugin-buble';
 import closure from 'rollup-plugin-closure-compiler-js';
 import filesize from 'rollup-plugin-filesize';
+import scss from 'rollup-plugin-scss';
 
 // you can use UglifyJS instead of Closure — the result will be ~3.3kb
 // import uglify from 'rollup-plugin-uglify';
 
-const plugins = [ nodeResolve(), commonjs(), svelte({ css: true }) ];
+const plugins = [ nodeResolve(), commonjs(), scss(), svelte() ];
 if ( process.env.production ) {
 	plugins.push(
 		buble(),
