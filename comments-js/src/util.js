71ec6dfb02037cc932d152542ce6f988e20a5c2b
dyu <david.yu.ftw@gmail.com>
2017-05-04 22:23:17 +0800

    move to utils, add button type

--- comments-js/src/util.js
+++ comments-js/src/util.js
@@ -10,7 +10,8 @@ converter.setFlavor('github')
 export const POST_ID = window['post_id']
 
 export const context = {
-    raw_items: []
+    raw_items: [],
+    items: []
 }
 
 function plural(num, unit) {
@@ -48,4 +49,55 @@ export function timeago(ts) {
 export function extractMsg(data) {
     //return Array.isArray(data) && data[1]['1'] || String(data)
     return Array.isArray(data) ? data[1]['1'] : String(data);
+}
+
+// ==================================================
+
+export function toPayload(name, content, reply) {
+    let items = !reply ? context.items : reply.children
+    // lastSeenKey
+    let prefix = !items.length ? '' : `"1":"${items[items.length - 1]['1']}",`
+    // parentKey
+    let suffix = !reply ? '' : `,"8":"${reply['1']}"`
+    return `{${prefix}"4":"${name}","5":"${content}","6":${POST_ID}${suffix}}`
+}
+
+export function toTree(raw_items, items) {
+    let last_item = !items.length ? null : items[items.length - 1],
+        last_depth = !last_item ? 0 : last_item.depth,
+        item,
+        depth
+    
+    for (var i = 0, len = raw_items.length; i < len; i++) {
+        item = raw_items[i]
+        item.parent = null
+        item.children = []
+        if (!(depth = item['7'])) {
+            items.push(item)
+            last_item = item
+            last_depth = depth
+            continue
+        }
+        if (depth === last_depth) {
+            (item.parent = last_item.parent).children.push(item)
+            last_item = item
+            continue
+        }
+        if (depth > last_depth) {
+            (item.parent = last_item).children.push(item)
+            last_item = item
+            last_depth = depth
+            continue
+        }
+
+        while (depth !== last_item.parent.depth) {
+            last_item = last_item.parent
+        }
+
+        (item.parent = last_item.parent).children.push(item)
+        last_item = item
+        last_depth = depth
+    }
+
+    return items
 }
