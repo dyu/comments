ac6aa7e8f0ea3b96aee9ad289fcfaf54541bab77
dyu <david.yu.ftw@gmail.com>
2017-06-22 07:26:47 +0800

    rt_flags for standalone version

--- scripts/s-start.sh
+++ scripts/s-start.sh
@@ -6,10 +6,12 @@ BIN=target/hprotostuffdb-rjre
 DATA_DIR=target/data/main
 JAR=comments-all/target/comments-all-jarjar.jar
 ASSETS='-Dprotostuffdb.assets_dir=comments-ts/'
+# serve root index.html when uri not found
+RT_FLAGS='-Dprotostuffdb.rt_flags=1'
 PUBSUB='-Dprotostuffdb.with_pubsub=true'
 ARGS=$(cat ARGS.txt)
 PORT=$(cat PORT.txt)
 
 echo "The app is available at http://127.0.0.1:$PORT"
-$BIN $PORT comments-ts/g/user/UserServices.json $ARGS $PUBSUB $ASSETS -Djava.class.path=$JAR comments.all.Main
+$BIN $PORT comments-ts/g/user/UserServices.json $ARGS $PUBSUB $ASSETS $RT_FLAGS -Djava.class.path=$JAR comments.all.Main
 
