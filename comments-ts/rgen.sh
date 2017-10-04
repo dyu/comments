28357142e9398bbe823b8f7ca1f6cd81a2d00277
dyu <david.yu.ftw@gmail.com>
2017-10-04 23:07:04 +0800

    gen html

---
+++ comments-ts/rgen.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# locate
+if [ ! -n "$BASH_SOURCE" ]; then
+    SCRIPT_DIR=`dirname "$(readlink -f "$0")"`
+else
+    F=$BASH_SOURCE
+    while [ -h "$F" ]; do
+        F="$(readlink "$F")"
+    done
+    SCRIPT_DIR=`dirname "$F"`
+fi
+
+cd $SCRIPT_DIR
+
+# master
+head --lines=-7 index.html > rmaster.html && \
+printf "        rpc_host: 'http://' + window.location.hostname + ':5020',\n        post_id: 1\n    }\n    </script><script src=\"/dist/build.js\"></script>\n    <link rel=\"stylesheet\" href=\"/dist/build.css\" />\n  </body>\n</html>\n" >> rmaster.html
+
+# slave
+head --lines=-17 index.html > rslave.html && \
+printf '    <script>(function(){\n' >> rslave.html && \
+cat rcommon.js >> rslave.html && \
+printf '    uri_w_map = ' >> rslave.html && \
+tr -d ' \t\n\r\f' < g/user/w/UserServices.json >> rslave.html && \
+printf '\n    appendEl("script", "src", "/dist/build.js")\n    })();</script>\n    <link rel="stylesheet" href="/dist/build.css" />\n  </body>\n</html>\n' >> rslave.html
---
+++ comments-ts/rslave.js
@@ -0,0 +1,42 @@
+    function appendEl(tag, key, val) {
+        var el = document.createElement(tag)
+        el[key] = val
+        document.body.appendChild(el)
+        return el
+    }
+    var protocol = window.location.protocol,
+        http = 0 === protocol.indexOf('http'),
+        hostname = http ? window.location.hostname : '127.0.0.1',
+        master_ip = window.master_ip,
+        master_prefix = '//' + (master_ip || hostname) + ':5020',
+        slave_prefix = '//' + hostname + (master_ip ? ':5020' : ':5025'),
+        rpc_config_d,
+        uri_w_map
+    
+    window.rpc_host = http ? protocol : 'http:'
+    window.rpc_config = {
+        get$$: function(uri, opts) {
+            if (!rpc_config_d) rpc_config_d = window['rpc_config_d']
+            
+            var prefix = uri_w_map[uri] ? master_prefix : slave_prefix
+            return rpc_config_d.get$$(prefix + uri, opts)
+        },
+        post$$: function(uri, data) {
+            if (!rpc_config_d) rpc_config_d = window['rpc_config_d']
+            
+            var prefix = uri_w_map[uri] ? master_prefix : slave_prefix
+            return rpc_config_d.post$$(prefix + uri, data)
+        }
+    }
+    window.comments_config = {
+        //limit_depth: 10,
+        //collapse_depth: 7,
+        //content_limit: 2048,
+        ui_flags: 5,
+        //auth_host: 'https://api.dyuproject.com',
+        //auth_flags: 0x77,
+        ws_enabled: true,
+        ws_reconnect_secs: 5,
+        ws_host: 'ws:' + slave_prefix,
+        post_id: 1
+    }
