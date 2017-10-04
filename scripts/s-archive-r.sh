b13bd8285c079beb06c09038f9d6a386e4590ace
dyu <david.yu.ftw@gmail.com>
2017-10-05 00:13:36 +0800

    archive script

---
+++ scripts/s-archive-r.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+[ -e scripts ] || { echo 'Execute this script from root dir.'; exit 1; }
+
+OUT_DIR=target/bin
+
+[ -e $OUT_DIR ] || mkdir -p $OUT_DIR
+
+RMASTER_FILE=$OUT_DIR/comments-linux-master-x64.tar.gz
+RSLAVE_FILE=$OUT_DIR/comments-linux-slave-x64.tar.gz
+
+TAR_ARGS='target/jre/*/'
+[ "$1" != "" ] && TAR_ARGS=$1
+
+echo "========== tar.gz"
+mv comments-ts/index.html /tmp/ || exit 1
+cp comments-ts/rmaster.html comments-ts/index.html || exit 1
+
+rm -f $RMASTER_FILE $RSLAVE_FILE
+
+echo '#!/bin/sh' > start.sh && \
+    printf 'BIN=target/hprotostuffdb-rmaster-rjre\n' >> start.sh && \
+    tail --lines=+6 scripts/s-start.sh >> start.sh && \
+    chmod +x start.sh && \
+    tar -cvzf $RMASTER_FILE start.sh target/hprotostuffdb-rmaster-rjre $TAR_ARGS -T scripts/files.txt
+
+echo '#!/bin/sh' > start.sh && \
+    printf '[ ! -n "$1" ] && echo "1st arg (ip:port of master) is required." && exit 1\nBIN=target/hprotostuffdb-rslave-rjre\nARGS_TXT=$(cat ARGS.txt)\nARGS="$ARGS_TXT -Dprotostuffdb.master=ws://$1"' >> start.sh && \
+    tail --lines=+6 scripts/s-start.sh >> start.sh && \
+    chmod +x start.sh && \
+    tar -cvzf $RSLAVE_FILE start.sh target/hprotostuffdb-rslave-rjre $TAR_ARGS -T scripts/files.txt
+
+rm -f start.sh
+mv /tmp/index.html comments-ts/
