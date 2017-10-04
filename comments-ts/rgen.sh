2c93d60f7c6486c1b24f1865d5a65ceafe1a63dc
dyu <david.yu.ftw@gmail.com>
2017-10-05 01:13:07 +0800

    check port 80

--- comments-ts/rgen.sh
+++ comments-ts/rgen.sh
@@ -15,7 +15,7 @@ cd $SCRIPT_DIR
 
 # master
 head --lines=-7 index.html > rmaster.html && \
-printf "        rpc_host: 'http://' + window.location.hostname + ':5020',\n        post_id: 1\n    }\n    </script><script src=\"/dist/build.js\"></script>\n    <link rel=\"stylesheet\" href=\"/dist/build.css\" />\n  </body>\n</html>\n" >> rmaster.html
+printf "        rpc_host: 'http://' + window.location.hostname + ('80' !== window.location.port && ':5020' || ''),\n        post_id: 1\n    }\n    </script><script src=\"/dist/build.js\"></script>\n    <link rel=\"stylesheet\" href=\"/dist/build.css\" />\n  </body>\n</html>\n" >> rmaster.html
 
 # slave
 head --lines=-17 index.html > rslave.html && \
